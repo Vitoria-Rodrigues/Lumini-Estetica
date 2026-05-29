bc0a0c707a1c4446127926fe12280591eb59f14f
Vitoria <vitoria.rf@outlook.com>
2026-05-29 19:18:32 -0300

    feat: create generic Table component and UI styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 
 import './styles/App.css';
 
+import Table from "@/components/ui/Table/Table";
+
 function App() {
 
   return (
     <div className="App">
       <h1>Lumini Estética</h1>
+      <Table />
     </div>
   )
 }
---
+++ src/components/ui/Table/Table.tsx
@@ -0,0 +1,37 @@
+import classes from "./Table.module.css";
+
+interface Column<T>{
+    label: string;
+    key: keyof T | 'actions';
+    render?: (item: T) => React.ReactNode;
+}
+
+interface TableProps<T>  {
+    columns: Column<T>[];
+    data: T[];
+}
+
+const Table = () => {
+  return (
+    <div className={classes.table_container}>
+       <table className={classes.table}>
+        <thead className={classes.table_head}>
+            <tr>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+            </tr>
+        </thead>
+        <tbody className={classes.table_body}>
+            <tr className={classes.table_line}>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+            </tr>
+        </tbody>
+        </table> 
+    </div>
+  )
+}
+
+export default Table
