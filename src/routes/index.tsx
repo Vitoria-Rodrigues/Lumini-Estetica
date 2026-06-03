993be4aa9dbfd8bd187d9934187d8db7d43c7130
Vitoria <vitoria.rf@outlook.com>
2026-06-03 17:29:39 -0300

    feat: add route to login page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import Home from "@/pages/Home/Home";
+import Login from "@/pages/Login/Login";
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />, 
+  },
+  {
+    path: '/login',
+    element: <Login />, 
   },
   {
     path: '*',
