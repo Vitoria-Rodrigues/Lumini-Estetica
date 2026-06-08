b49ea128ca9efdb709a3ec993864c75f93f70624
Vitoria <vitoria.rf@outlook.com>
2026-06-08 16:19:41 -0300

    feat: add index.ts to centralize exports

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
-import Home from "@/pages/Home/Home";
-import Login from "@/pages/Login/Login";
+import { Home, Login } from "@/pages"
 
 export const router = createBrowserRouter([
   {
