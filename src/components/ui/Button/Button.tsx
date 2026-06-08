3e62a6d043f68cb9b22b2d7597d0965e7be889e2
Vitoria <vitoria.rf@outlook.com>
2026-06-08 14:55:34 -0300

    feat: add icon support to the component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,14 +1,16 @@
 import type { CSSProperties } from "react";
+import type { IconType } from "react-icons";
 import classes from "./Button.module.css";
 
 type ButtonProps = {
     title: string;
     padding?: string;
     width?: string;
+    icon?: IconType;
     onClick?: () => void;
 }
 
-const Button = ({ title, padding, width, onClick}: ButtonProps) => {
+const Button = ({ title, padding, width, icon: Icon, onClick}: ButtonProps) => {
   const dynamicStyle = {
     "--btn-padding": padding,
     "--btn-width": width
@@ -18,7 +20,8 @@ const Button = ({ title, padding, width, onClick}: ButtonProps) => {
     <button className={classes.btn_principal} 
       style={dynamicStyle}
       onClick={onClick}>
-      {title}
+      {Icon && <Icon className={classes.btn_icon} />}
+      <span>{title}</span>
     </button>
   )
 }
