5ecf79472d54a781f124bc8ffe97808df07195cb
Vitoria <vitoria.rf@outlook.com>
2026-07-30 17:26:03 -0300

    feat: configure ToasterProvider and render Toaster component globally

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,17 @@ import { router } from './routes';
 //Auth
 import { AuthProvider } from './contexts/AuthContext';
 
+//Toaster
+import { ToasterProvider } from './contexts/ToasterContext';
+import { Toaster } from './components/ui';
+
 export default function App() {
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      <ToasterProvider>
+        <Toaster />
+        <RouterProvider router={router} />
+      </ToasterProvider>
     </AuthProvider>
   );
 }
