7a85f371545d3c988b150087ce4345f249bda09b
Vitoria <vitoria.rf@outlook.com>
2026-08-27 17:42:44 -0300

    refactor: adjust phone number mask pattern

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -17,9 +17,9 @@ export const formatPhone = (phone: string | undefined | null): string => {
     const digits = cleanDigits(phone);
 
     if(digits.length === 11){
-        return digits.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+        return digits.replace(/(\d{2})(\d{5})(\d{4})/, "($1)$2-$3");
     } else if(digits.length === 10) {
-        return digits.replace(/(\d{2})(\d{4})(\d{4})/, "($1) $2-$3");
+        return digits.replace(/(\d{2})(\d{4})(\d{4})/, "($1)$2-$3");
     }
 
     return phone;
